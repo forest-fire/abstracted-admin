02f911fc869b282f16f2bdc7ec0094871f6d1889
Ken Snyder <ken@ken.net>
2017-07-13 23:33:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+[![Build Status](https://travis-ci.org/forest-fre/abstracted-admin.svg?branch=master)](https://travis-ci.org/forest-fre/abstracted-admin.svg?branch=master)
+[![Coverage Status](https://coveralls.io/repos/github/forest-fre/abstracted-admin/badge.svg?branch=master)](https://coveralls.io/github/forest-fre/abstracted-admin?branch=master)
+[![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT)
+
+# Abstracted Admin
+> A minimal abstraction over the Firebase ADMIN API
+
+### Usage 
+Meant for backend nodejs micro-services which interact with Firebase's Admin API using a "service policy" to authenticate.
+
+````typescript
+import DB from 'abstracted-admin';
+const db = new DB();
+// Get a list of records
+const users = await db.getValue<IUser[]>('users');
+// Push a new value onto a list
+const company: ICompany = {
+  name: "Acme",
+  employees: 500
+}
+db.push<ICompany>('/companies', company);
+````
+All of the Authentication is done transparently as soon as requests are made to the database. In order for this library to achieve this it will need the following environment variables set:
+
+- `FIREBASE_SERVICE_ACCOUNT` - this should be a URI-Encoded string of the JSON data which you exported at the time you created a Service Account on Google.
+- `FIREBASE_DATA_ROOT_URL` - comes from the Firebase console and dictates which DB to connect to
+
---
+++ src/db.ts
@@ -0,0 +1,178 @@
+import * as firebase from 'firebase-admin';
+import { IDictionary } from 'common-types';
+import * as convert from 'typed-conversions';
+import moment = require('moment');
+import * as process from 'process';
+
+export enum FirebaseBoolean {
+  true = 1, 
+  false = 0 
+}
+
+export type Snapshot = firebase.database.DataSnapshot;
+export type Reference = firebase.database.Reference;
+
+const findRecord = {
+
+  first: (hash: IDictionary) => {
+    const exists = Object.keys(hash).length > 0;
+    return exists
+      ? hash[Object.keys(hash)[0]]
+      : undefined;
+  },
+
+  mostRecent: (hash: IDictionary) => {
+    const exists = Object.keys(hash).length > 0;
+    return exists
+      ? hash[Object.keys(hash).sort()[0]]
+      : undefined;
+  },
+
+  oldest: (hash: IDictionary) => {
+    const exists = Object.keys(hash).length > 0;
+    return exists
+      ? hash[Object.keys(hash).sort().reverse()[0]]
+      : undefined;
+  },
+};
+
+export default class DB {
+  private static isConnected: boolean = false;
+  private static isAuthorized: boolean = false;
+  private static connection: firebase.database.Database;
+  public auth: firebase.auth.Auth;
+
+  constructor(debugging = false) {
+    this.connect(debugging);
+    this.auth = firebase.auth();
+    DB.connection = firebase.database();
+    firebase.database().goOnline();
+    firebase.database().ref('.info/connected').on('value', (snap) => {
+      DB.isConnected = snap.val();
+    });
+  }
+
+  /** Get a DB reference for a given path in Firebase */
+  public ref(path: string): firebase.database.Reference {
+    return DB.connection.ref(path);
+  }
+
+  /** set a "value" in the database at a given path */
+  public async set(path: string, value: any): Promise<void> {
+    return this.ref(path).set(value)
+      .catch(e => this.handleError(e, 'set', `setting value @ "${path}"`));
+  }
+
+  public async update<T = IDictionary>(path: string, value: T) {
+    return this.ref(path).update(value);
+  }
+
+  public async remove(path: string, ignoreMissing = false): Promise<void> {
+    const ref = this.ref(path); 
+    
+    return ref.remove()
+      .catch(e => {
+        if (ignoreMissing && e.message.indexOf('key is not defined') !== -1) {
+          return Promise.resolve();
+        }
+         
+        this.handleError(
+          e, 
+          'remove', 
+          `attempt to remove ${path} failed: `
+        );
+      });
+  }
+
+  public async getSnapshot(path: string): Promise<firebase.database.DataSnapshot> {
+    return this.ref(path).once('value');
+  }
+
+  /** returns the value at the given path in the database */
+  public async getValue<T = any>(path: string): Promise<T> {
+    const snap = await this.getSnapshot(path);
+    return snap.val() as T;
+  }
+
+  /**
+   * Gets a snapshot from a given path in the DB
+   * and converts it to a JS object where the key 
+   * is converted to be the "id" property of the 
+   * object.
+   */
+  public async getRecord<T = any>(path: string): Promise<T> {
+    return this.getSnapshot(path)
+      .then(snap => convert.snapshotToHash(snap) as T);
+  }
+
+  /** 
+   * Pushes a value (typically a hash) under a given path in the 
+   * database but allowing Firebase to insert a unique "push key" 
+   * to ensure the value is placed into a Dictionary/Hash structure
+   * of the form of "/{path}/{pushkey}/{value}"
+   */
+  public async push<T = IDictionary<any>>(path: string, value: T) {
+    return this.ref(path).push(value);
+  }
+
+  /** validates the existance of a path in the database */
+  public async exists(path: string): Promise<boolean> {
+    return this.getSnapshot(path)
+      .then(snap => snap.val() ? true : false);
+  }
+
+  private handleError(e: any, name: string, message = '') {
+      console.error(`Error ${message}:`, e);
+      return Promise.reject({
+        code: `firebase/${name}`,
+        message: message + e.message || e
+      });
+  }
+
+  private connect(debugging: boolean = false): void {
+    
+    if (!DB.isAuthorized) {
+      if (!process.env['AWS_STAGE']) {
+        throw new Error('The AWS_STAGE variable was not set!');
+      }
+      const serviceAcctEncoded = process.env['FIREBASE_SERVICE_ACCOUNT'];      
+      if (!serviceAcctEncoded) {
+        throw new Error('Problem loading the credientials for Firebase admin API');
+      }
+      
+      const serviceAccount: firebase.ServiceAccount = JSON.parse(
+        Buffer
+          .from(process.env['FIREBASE_SERVICE_ACCOUNT'], 'base64')
+          .toString()
+      );
+        
+      console.log(
+        `Connecting to Firebase: [${process.env['FIREBASE_DATA_ROOT_URL']}]`
+      );
+      try { 
+        firebase.initializeApp({
+          credential: firebase.credential.cert(serviceAccount),
+          databaseURL: process.env['FIREBASE_DATA_ROOT_URL']
+        });
+        DB.isAuthorized = true;
+      } catch (err) {
+        if (err.message.indexOf('The default Firebase app already exists.') !== -1) {
+          console.warn('DB was already logged in, however flag had not been set!');
+          DB.isConnected = true;
+        } else {
+          DB.isConnected = false;
+          console.warn('Problem connecting to Firebase', err);
+          throw new Error(err);
+        }
+      }
+    } 
+
+    if (debugging) {
+      console.log('Firebase debugging enabled');
+      
+      firebase.database.enableLogging((message) => {
+        console.log("[FIREBASE]", message);
+      });
+    }
+  }
+}
