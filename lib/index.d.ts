454d971602d650714fea3f5a06cfcc9ca7fe1e99
Ken Snyder <ken@ken.net>
2018-02-05 21:02:43 -0800

    0.8.4

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 import { DB } from "./db";
 export default DB;
 export { DB, IFirebaseConfig, IFirebaseListener, FirebaseBoolean, Snapshot, Query, Reference } from "./db";
+export { RealTimeDB } from "abstracted-firebase";
