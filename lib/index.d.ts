3fc9d755af0bbca686742ee85669cc728cbfd2bb
Ken Snyder <ken@ken.net>
2018-02-23 10:32:59 -0800

    update to patched abstracted-firebase

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import { DB } from "./db";
+export { SerializedQuery } from "serialized-query";
 export default DB;
 export { DB, IFirebaseConfig, IFirebaseListener, FirebaseBoolean, Snapshot, Query, Reference } from "./db";
-export { RealTimeDB } from "abstracted-firebase";
