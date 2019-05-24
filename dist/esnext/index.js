fb03c2322991aa2c4ef5962a6036ea43571db880
Ken Snyder <ken@ken.net>
2019-05-23 20:35:39 -0700

    minor changes

--- dist/esnext/index.js
+++ dist/esnext/index.js
@@ -1,3 +1,4 @@
 export { SerializedQuery } from "serialized-query";
 export { DB } from "./db";
 export { RealTimeDB, FirebaseBoolean } from "abstracted-firebase";
+// export { DataSnapshot, Query, Reference } from "@firebase/database-types";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { SerializedQuery } from "serialized-query";
 export { DB, IFirebaseListener } from "./db";
 export { RealTimeDB, FirebaseBoolean, IFirebaseConfig } from "abstracted-firebase";
-export { DataSnapshot, Query, Reference } from "@firebase/database-types";
+// export { DataSnapshot, Query, Reference } from "@firebase/database-types";
