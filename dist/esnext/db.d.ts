dfe8df59750533457d33a4f1aa58ac94a7b6009e
Ken Snyder <ken@ken.net>
2020-04-21 21:05:05 -0700

    bumped to 0.38.x series

--- dist/esnext/db.d.ts
+++ dist/esnext/db.d.ts
@@ -24,6 +24,12 @@ export declare class DB extends RealTimeDB<firebase.auth.Auth> {
     /**
      * Provides access to the Firebase Admin Auth API.
      *
+     * > If using a _mocked_ database then the Auth API will be redirected to **firemock**
+     * instead of the real Admin SDK for Auth. Be aware that this mocked API may not be fully implemented
+     * but PR's are welcome if the part you need is not yet implemented. If you want to explicitly state
+     * whether to use the _real_ or _mock_ Auth SDK then you can state this by passing in a `auth` parameter
+     * as part of the configuration (using either "real" or "mocked" as a value)
+     *
      * References:
      * - [Introduction](https://firebase.google.com/docs/auth/admin)
      * - [API](https://firebase.google.com/docs/reference/admin/node/admin.auth.Auth)
